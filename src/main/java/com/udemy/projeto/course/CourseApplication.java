531e7fecea0ece6a6924ac9864fc0335b22901c7
Ricardo Alves <ricardo.justino@gmail.com>
2022-08-22 22:53:26 -0300

    User entity and resource

--- src/main/java/com/udemy/projeto/course/CourseApplication.java
+++ src/main/java/com/udemy/projeto/course/CourseApplication.java
@@ -1,5 +1,6 @@
 package com.udemy.projeto.course;
 
+import com.udemy.projeto.course.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,5 @@ public class CourseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
-
+	public User user;
 }
