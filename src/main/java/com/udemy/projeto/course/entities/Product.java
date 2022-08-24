aeda8cc755c6aab77bc006aae119185a031663a5
Ricardo Alves <ricardo.justino@gmail.com>
2022-08-23 23:40:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/udemy/projeto/course/entities/Product.java
+++ src/main/java/com/udemy/projeto/course/entities/Product.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
@@ -17,7 +18,11 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
